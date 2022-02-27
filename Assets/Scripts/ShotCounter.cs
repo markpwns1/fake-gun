9e62587055677ede77eb0f4bae6cf200cdc263d0
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2022-02-26 19:28:16 -0700

    canvas added

--- Assets/Scripts/ShotCounter.cs
+++ Assets/Scripts/ShotCounter.cs
@@ -7,14 +7,35 @@ public class ShotCounter : MonoBehaviour
 {
     public Gun gun;
     public GameObject warningText;
+
+    IEnumerator FlashingCoroutine()
+    {
+        while (true)
+        {
+            warningText.GetComponent<Text>().color = Color.white;
+            yield return new WaitForSeconds(0.5f);
+            warningText.GetComponent<Text>().color = Color.red;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
     void Start()
     {
         gun = FindObjectOfType<Gun>();
         warningText = GameObject.FindGameObjectWithTag("Warning");
+        warningText.SetActive(false);
     }
 
     void Update()
     {
         GetComponent<Text>().text = (gun.clipSize - gun.BulletShot()).ToString() + " / " + gun.clipSize;
+        if (gun.BulletShot() >= gun.clipSize)
+        {
+            warningText.SetActive(true);
+            var c = StartCoroutine(FlashingCoroutine());
+        }
+        else {
+            warningText.SetActive(false);
+        }
     }
 }
